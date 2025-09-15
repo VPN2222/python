8ee9e93116cfda775edcf1b0cdf75e6a3a779bc1
VPN2222 <159782382+VPN2222@users.noreply.github.com>
2025-09-15 17:07:19 +0500

    Create script.py

---
+++ lesson-1/homework/script.py
@@ -0,0 +1,28 @@
+a =float(input("Kvadarati tomonini kiriting"))
+perimetr=4a
+area=aa
+print("Perimetri:"+perimetr)
+print("tuzasi :"+area)
+
+import math
+d=float(input("Natija kiriting"))
+c=math.pi*d
+print("natija :"+d)
+
+a=float(input("son kiriting : "))
+b=float(input("son kiriting : "))
+r=(a+b)/2
+print("natija : "+r)
+
+a1=float(input("son kiriting : "))
+b1=float(input("son kiriting : "))
+
+summa = a1 + b1
+product = a1 * b1
+square_a = a1 ** 2
+square_b = b1 ** 2
+
+print("Yig'indisi:", summa)
+print("Ko'paytmasi:", product)
+print("a sonining kvadrati:", square_a)
+print("b sonining kvadrati:", square_b)
